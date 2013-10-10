f116254b0c8b25607008a7668cecd06573efa75f
Sam Crang <sam.crang@7digital.com>
2013-10-10 12:15:11 +0100

    Hack around a bug(?) in Vagrant.
    https://github.com/mitchellh/vagrant/issues/2333

--- lib/kitchen/vagrant/vagrantfile_creator.rb
+++ lib/kitchen/vagrant/vagrantfile_creator.rb
@@ -52,7 +52,6 @@ module Kitchen
         arr << %{  c.vm.box = "#{config[:box]}"}
         arr << %{  c.vm.box_url = "#{config[:box_url]}"} if config[:box_url]
         arr << %{  c.vm.synced_folder ".", "/vagrant", disabled: true}
-        arr << %{  c.vm.hostname = "#{instance.name}.vagrantup.com"}
         arr << %{  c.ssh.username = "#{config[:username]}"} if config[:username]
       end
 
